f7b9ec8f8e57990cc93392f6eca868aada36015a
Sankar Narayan Behera <94392sankar99298@gmail.com>
2025-10-03 00:44:20 +0530

    Added Code.txt and Output.txt

---
+++ Practice.py
@@ -0,0 +1,9 @@
+def check_number(my_number):
+    if my_number > 10:
+        print("Values are greater then 10")
+    else:
+        print("Values are lesser then 10")
+
+my_number = 8
+
+check_number(my_number)
